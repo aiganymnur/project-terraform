62e4c74851e5c86064a9a9b9596d38a809a8ba45
aiganymnur <aiganymnurgazina@gmail.com>
2025-02-21 03:44:57 +0000

    changed

--- 1st_try/apache.sh
+++ 1st_try/apache.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-
-sudo yum install wget php-mysqlnd httpd php-fpm php-mysqli mariadb105-server php-json php php-devel -y
+ 
+sudo yum install httpd -y
 sudo systemctl start httpd
 sudo systemctl enable httpd
 wget https://wordpress.org/latest.tar.gz
 tar zxf latest.tar.gz
-
-
+sudo dnf install wget php-mysqlnd httpd php-fpm php-mysqli mariadb105-server php-json php php-devel -y
 sudo mv wordpress/* /var/www/html
-sudo chown -R  /var/www/html
-sudo chmod 755 /var/www/html
+sudo systemctl restart httpd
 sudo rm -rf /var/www/html/index.html
 
-sudo systemctl restart httpd
-
+sudo systemctl start mariadb
+sudo systemctl enable mariadb
